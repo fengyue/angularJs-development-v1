17b88a84737eca944ca623bb5f9871b75a73cd36
DESKTOP-FSNB60T\daime <815642343@qq.com>
2016-10-29 02:57:41 +0800

    step2 half completed

---
+++ app3/app/js/app.js
@@ -0,0 +1,19 @@
+var bookStoreApp = angular.module('bookStoreApp', [
+    'ngRoute', 'ngAnimate', 'bookStoreCtrls', 'bookStoreFilters',
+    'bookStoreServices', 'bookStoreDirectives'
+]);
+
+bookStoreApp.config(function($routeProvider) {
+    $routeProvider.when('/hello', {
+        templateUrl: 'tpls/hello.html',
+        controller: 'HelloCtrl'
+    }).when('/list',{
+    	templateUrl:'tpls/bookList.html',
+    	controller:'BookListCtrl'
+    }).when('/play',{
+        templateUrl:'tpls/play.html',
+        controller:'PlayCtrl'
+    }).otherwise({
+        redirectTo: '/hello'
+    })
+});
